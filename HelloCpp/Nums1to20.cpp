a6c0fe012228c5aa6da2166ca54fb7d27b079ab4
ivogul <ivogul@gmail.com>
2017-09-28 17:38:07 +0300

    Initial commit C++

---
+++ HelloCpp/Nums1to20.cpp
@@ -0,0 +1,20 @@
+// Expression.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	/*long result;
+	result = (3522 + 52353) * 23 - (2336 * 501 + 23432 - 6743) * 3;
+	cout << result << endl;*/
+
+	cout << 1 << endl;
+	cout << 2 << endl;
+
+	return 0;
+}
+
